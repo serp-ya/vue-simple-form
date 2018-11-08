1c9cd39f00bf5e458b900b31ec4150dbaaf1050d
Sergei Kundriukov <Sergei_Kundriukov@epam.com>
2018-11-08 12:22:24 +0300

    The logic of fullname field updated

--- src/components/FullNameField.vue
+++ src/components/FullNameField.vue
@@ -1,19 +1,60 @@
 <template>
-  <div class="form-group">
-    <label for="fullName">Fullname</label>
-    <input 
-      type="text" 
-      class="form-control" 
-      id="fullName" 
-      placeholder="Fullname"
-      :value="value"
-      @input="$emit('input', $event.target.value);"
-    />
+  <div>
+    <div class="form-group">
+      <label for="firstName">First Name</label>
+      <input 
+        type="text" 
+        class="form-control" 
+        id="firstName" 
+        placeholder="First Name"
+        :value="firstName"
+        @input="input($event, 'firstName')"
+      />
+    </div>
+    
+    <div class="form-group">
+      <label for="lastName">Last Name</label>
+      <input 
+        type="text" 
+        class="form-control" 
+        id="lastName" 
+        placeholder="Last Name"
+        :value="lastName"
+        @input="input($event, 'lastName')"
+      />
+    </div>
   </div>
 </template>
 
 <script>
   export default {
+    props: {
+      value: {
+        type: String,
+        default: '',
+      },
+    },
+    methods: {
+      splitValues() {
+        return this.value.split(' ');
+      },
+      input(event, fieldName) {
+        const { value } = event.target;
+        const result = [
+          fieldName === 'firstName' ? value : this.firstName,
+          fieldName === 'lastName' ? value : this.lastName,
+        ].join(' ');
+        this.$emit('input', result);
+      }
+    },
+    computed: {
+      firstName() {
+        return this.splitValues()[0] || '';
+      },
+      lastName() {
+        return this.splitValues()[1] || '';
+      }
+    }
   }
 </script>
 
